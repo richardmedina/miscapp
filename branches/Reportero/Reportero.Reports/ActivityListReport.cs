b8b88d32046d0de73441a6f0043209d8f50e7a42
ricki9@gmail.com <ricki9@gmail.com@6d5072ba-9bd0-11de-9070-0b7171dc4b6e>
2009-10-19 19:58:45 +0000

    * Reportero.sln: * Reportero/Main.cs: * Reportero.userprefs: * Reportero/Reportero.pidb: * Reportero.UI/Reportero.UI.pidb: * Reportero.Reports/LoadingWindow.cs: * Reportero.Data/Reportero.Data.pidb: * Reportero.UI/Widgets/ReportChooser.cs: * Reportero.Reports/ActivityListReport.cs: * Reportero.Reports/Reportero.Reports.pidb: * Reportero.Reports/Reportero.Reports.csproj:

--- branches/Reportero/Reportero.Reports/ActivityListReport.cs
+++ branches/Reportero/Reportero.Reports/ActivityListReport.cs
@@ -1,9 +1,13 @@
 
 using System;
 using System.IO;
+using System.Reflection;
+using System.Threading;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using Reportero.Data;
+
+//using Gtk;
 
 namespace Reportero.Reports
 {
@@ -12,6 +16,7 @@ namespace Reportero.Reports
 	public class ActivityListReport : ActivityReport
 	{
 		private Leadership _leadership;
+		private LoadingWindow _loader;
 		
 		public ActivityListReport (Leadership leadership)
 			: this (leadership, DateTime.Now, DateTime.Now)
@@ -22,67 +27,129 @@ namespace Reportero.Reports
 			: base (start, end)
 		{
 			Leader = leadership;
+			_loader = new LoadingWindow ();
 		}
-		
-		protected override void OnShown ()
+				
+		public void CreatePdf (string filename)
 		{
-			createPdf ();
+			Thread thread = new Thread ((ThreadStart) delegate {
+				update (0);
+				createPdf (filename);
+			});
+			
+			thread.Start ();
 		}
 		
-		private void createPdf ()
+		private void createPdf (string filename) 
 		{
 			Document doc = new Document (PageSize.LETTER);
 			PdfWriter writer = PdfWriter.GetInstance (doc,
-				new FileStream ("/home/richard/Desktop/file.pdf", FileMode.Create));
+				new FileStream (filename, FileMode.Create));
+
+			Font font_title = FontFactory.GetFont ("Comic sans ms", "UTF8", false, 16, 0, new Color (0x44, 0x44, 0x44));
+			Font font_sub1 = FontFactory.GetFont ("Arial", "UTF8", false, 14, 1, new Color (0x00, 0, 0));
+			Font font_sub2 = FontFactory.GetFont ("Arial", "UTF8", false, 12, 1, new Color (0, 0x00, 0));
+			Font font_sub3 = FontFactory.GetFont ("Arial", "UTF8", false, 10, 0, new Color (0, 0x00, 0));
+
+			Assembly a = Assembly.GetExecutingAssembly ();
+			Stream stream = a.GetManifestResourceStream ("reportero_icon_pep.png");
+			Image img = Image.GetInstance (stream);
+			img.Alignment = Image.LEFT_BORDER | Image.TEXTWRAP;
+			img.ScalePercent (70);
+
+			Paragraph head_para = new Paragraph ();
+			
+			head_para.Add (new Paragraph ("PEMEX EXPLORACION Y PRODUCCION", font_sub1));
+			head_para.Add (new Paragraph ("Región Sur", font_sub1));
+			head_para.Add (new Paragraph ("Activo Integral Samaria-Luna", font_sub2));
+			head_para.Add (new Paragraph ("Tecnología de Información", font_sub2));
+			head_para.Add (new Paragraph ("Reporte de Actividad Vehicular por Coordinación", font_sub2));
+			
+			HeaderFooter header = new HeaderFooter (head_para, false);
+			header.Alignment = HeaderFooter.ALIGN_CENTER;
+			doc.Header = header;
+
 			doc.Open ();
 			
 			doc.AddAuthor ("Software Reportero desarrollado por Ricardo Medina <rmedinalor@pep.pemex.com>");
-			doc.AddCreator ("Software Reportero desarrollado por Ricardo Medina <rmedinalo@pep.pemex.com>");
+			doc.AddCreator ("Software Reportero desarrollado por Ricardo Medina <rmedinalo@pep.pemex.com>");						
 			
-			Font font_title = FontFactory.GetFont ("Comic sans ms", "UTF8", false, 16, 0, new Color (0x44, 0x44, 0x44));
-			Font font_sub1 = FontFactory.GetFont ("Arial", "UTF8", false, 14, 0, new Color (0x00, 0, 0));
-			Font font_sub2 = FontFactory.GetFont ("Arial", "UTF8", false, 12, 0, new Color (0, 0x00, 0));
-			Font font_sub3 = FontFactory.GetFont ("Arial", "UTF8", false, 10, 0, new Color (0, 0x00, 0));
+			doc.Add (new Paragraph (Leader.Name, font_sub1));
 			
-			foreach (Leadership leader in Leadership.FromDatabase (Leader.Db)) {
-				doc.Add (new Paragraph (leader.Name, font_sub1));
-					foreach (VehicleUser vehicle in leader.GetVehicles ()) {
-						Paragraph para = new Paragraph ();
-						para.Add (new Phrase ("Vehículo. ", font_sub2));
-						para.Add (new Phrase (vehicle.VehicleId, font_sub2));
-						doc.Add (para);
-						para= new Paragraph ();
-						para.Add(new Phrase ("Asignado a. ", font_sub2));
-						para.Add (new Phrase (vehicle.Name, font_sub2));
-						doc.Add (para);
+			VehicleUserCollection vehicles = Leader.GetVehicles ();
+			
+			int counter = 0;
+			foreach (VehicleUser vehicle in vehicles) {
+				counter ++;
+				//update ("", (100 / vehicles.Count) * counter);
+				Console.WriteLine (((double)100 / (double)vehicles.Count) * (double)counter);
+				update ((100 / vehicles.Count) * counter);
+				
+				Paragraph para = new Paragraph ();						
+				para.Add (new Phrase ("Vehículo. ", font_sub2));
+				para.Add (new Phrase (vehicle.VehicleId, font_sub3));
+				doc.Add (para);
+				
+				para= new Paragraph ();
+				para.Add(new Phrase ("Asignado a. ", font_sub2));
+				para.Add (new Phrase (vehicle.Name, font_sub3));
+				
+				doc.Add (para);
 						
-						doc.Add (new Paragraph ("Detalles", font_sub3));
+				doc.Add (new Paragraph ("Detalles", font_sub2));
 						
-						for (DateTime i = StartingDate; i <= EndingDate; i = i.AddDays (1)) {
-							para = new Paragraph ();
-							
-							TimeSpan time = TimeSpan.FromMinutes (vehicle.GetMinutesRunning (i));
-							string detail = string.Format ("     {0}. {1} ", 
-								i.ToString ("dd-MM-yyyy"), time.ToString ());
-								
-							
-							para.Add (new Phrase (detail, font_sub1));
-							doc.Add (para);
-							
-						}
-					}
+				int minutes_total = 0;
+				int totaldays = (EndingDate - StartingDate).Days;
+				for (int i = 0; i <= totaldays; i ++) {
+					DateTime current_date = StartingDate.AddDays (i);
+					para = new Paragraph ();
+					int minutes = vehicle.GetMinutesRunning (current_date);
+					minutes_total += minutes;
+					
+					
+					TimeSpan time = TimeSpan.FromMinutes (minutes);
+					string detail = string.Format ("     {0}. {1} ", 
+						current_date.ToString ("dd-MM-yyyy"), time.ToString ());
+					
+					para.Add (new Phrase (detail, font_sub3));
+					doc.Add (para);
+				}
+				
+				int seconds_avrg = (minutes_total*60)/(totaldays+1);//(totaldays>1?totaldays+1:1);
+				TimeSpan total = TimeSpan.FromMinutes (minutes_total);
+				
+				para = new Paragraph (string.Format ("Actividad Total del Vehículo. {0}", total), font_sub2);
+				doc.Add (para);
+				para = new Paragraph (string.Format ("Actividad Promedio por día. {0}", TimeSpan.FromSeconds (seconds_avrg)), font_sub2);
+				doc.Add (para);
+				doc.Add (new Paragraph (" ", font_sub2));
 			}
-			
-			
-			//doc.Add (table);
+			doc.Add (new Paragraph (string.Format ("{0} Vehiculos contabilizados.", vehicles.Count), font_sub2));
 			doc.Close ();
 			writer.Close ();
+		}
+		
+		private void update (double percent)
+		{
+			update (string.Format ("{0}%", percent), percent);
+		}
+		
+		private void update (string progress_text, double percent)
+		{
+			Gtk.ThreadNotify notify = new Gtk.ThreadNotify (delegate {
+				_loader.ShowAll ();
+				_loader.ProgressText = progress_text;
+				_loader.Fraction = percent / 100;
+			});
+			
+			notify.WakeupMain ();
 		}
 		
 		private void createPdfTest ()
 		{
 			Document doc = new Document (PageSize.LETTER);
-			PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("/home/richard/Desktop/Chap0109.pdf", FileMode.Create));
+			PdfWriter writer = PdfWriter.GetInstance(doc, 
+				new FileStream("/home/richard/Desktop/Chap0109.pdf", FileMode.Create));
 			doc.Open ();
 			
 			Table table = new Table (2);
