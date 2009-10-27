439dedb5b00af932fc8e19354f1089791da5f87e
ricki9@gmail.com <ricki9@gmail.com@6d5072ba-9bd0-11de-9070-0b7171dc4b6e>
2009-10-27 20:57:39 +0000

    * Reportero.userprefs: * Reportero.Data/Leadership.cs: * Reportero.UI/Reportero.UI.pidb: * Reportero.Data/Reportero.Data.pidb: * Reportero.UI/Widgets/ReportChooser.cs: * Reportero.Reports/ActivityListReport.cs: * Reportero.Reports/Reportero.Reports.pidb:

--- branches/Reportero/Reportero.Reports/ActivityListReport.cs
+++ branches/Reportero/Reportero.Reports/ActivityListReport.cs
@@ -75,7 +75,6 @@ namespace Reportero.Reports
 			head_para.Add (new Paragraph ("PEMEX EXPLORACION Y PRODUCCION", font_title));
 			head_para.Add (new Paragraph ("Región Sur", font_sub1));
 			head_para.Add (new Paragraph ("Activo Integral Samaria-Luna", font_sub2));
-			//head_para.Add (new Paragraph ("Tecnología de Información", font_sub2));
 			head_para.Add (new Paragraph ("Reporte de Actividad Vehicular por Día", font_sub2));
 			
 			HeaderFooter header = new HeaderFooter (head_para, false);
@@ -86,11 +85,22 @@ namespace Reportero.Reports
 			
 			doc.AddAuthor ("Software Reportero desarrollado por Ricardo Medina <rmedinalor@pep.pemex.com>");
 			doc.AddCreator ("Software Reportero desarrollado por Ricardo Medina <rmedinalo@pep.pemex.com>");						
-			//Paragraph para = new Paragraph (
-			//	string.Format ("{0}. {1}", Leader.Name, Leader.GetFullname ()), font_sub1);
-			//doc.Add (para);
+			
+			int col = 0;
+			int row = 0;
 			
 			VehicleUserCollection vehicles = Leader.GetVehicles ();
+
+			Table table = new Table (6);
+			table.Padding = 5;
+			
+			Cell cell = createCell (Leader.Name);
+			table.AddCell (cell, row, 0);
+			
+			cell = createCell (Leader.GetFullname ());
+			cell.Colspan = 5;
+			table.AddCell (cell, row++, 1);
+			
 			
 			int counter = 0;
 			foreach (VehicleUser vehicle in vehicles) {
@@ -99,51 +109,43 @@ namespace Reportero.Reports
 				counter ++;
 				double percent = ((double) 100 / (double) vehicles.Count) * (double) counter;
 				_loader.AsyncUpdate ((int) percent);
-
-				Table table = new Table (6);
-				table.Padding = 5;
 			
-				Cell cell = createCell (Leader.Name);
-				table.AddCell (cell, 0, 0);
-			
-				cell = createCell (Leader.GetFullname ());
-				cell.Colspan = 5;
-				table.AddCell (cell, 0, 1);
-			
-				table.AddCell (createCell ("Vehículo"), 1, 0);
+				table.AddCell (createCell ("Vehículo"), row, 0);
 				cell = createCell (vehicle.VehicleId);
 				cell.Colspan = 2;
-				table.AddCell (cell, 1, 1);
+				table.AddCell (cell, row, 1);
 				
-				table.AddCell (createCell ("Asignado a"), 1, 3);
+				table.AddCell (createCell ("Asignado a"), row, 3);
 				cell = createCell (vehicle.Name);
 				cell.Colspan = 2;
-				table.AddCell (cell, 1, 4);
+				table.AddCell (cell, row ++, 4);
 
-				table.AddCell (createCell ("Detalles"), 2, 0);
+				table.AddCell (createCell ("Detalles"), row, 0);
 				cell = createCell ("");
-				cell.Colspan = 5;
-				table.AddCell (cell);
+				cell.Colspan = 3;
+				table.AddCell (cell, row++, 1);
 																										
 				int minutes_total = 0;
 				int totaldays = (EndingDate - StartingDate).Days;
 				
 				int x = 0;
-				for (x = 0; x < 3 && x < totaldays+1; x ++) {
-					table.AddCell (createCell ("Fecha"), 3, (x*2));
-					table.AddCell (createCell ("Actividad"), 3, (x*2) + 1);
+				for (x = 0; x < 3 && x < totaldays + 1; x ++) {
+					table.AddCell (createCell ("Fecha"), row, (x * 2));
+					table.AddCell (createCell ("Actividad"), row, (x * 2) + 1);
 				}
-				
-				int row = 0;
 				
 				for (int i = 0; i <= totaldays; i ++) {
 					if (_canceled)
 						break;
-						
-					int col = i % 3;
+					
+					percent += ((double) 100 / (double) vehicles.Count) * (double) (((double)counter/(double)totaldays) * (double)i);
+					_loader.AsyncUpdate ((int) percent);
+					
+					col = i % 3;
 					if (col == 0)
 						row ++;
-						
+					
+					Console.WriteLine ("{0},{1}", row, col);
 					DateTime current_date = StartingDate.AddDays (i);
 					
 					int minutes = vehicle.GetMinutesRunning (current_date);
@@ -154,15 +156,15 @@ namespace Reportero.Reports
 						current_date.ToString ("dd-MM-yyyy"), time.ToString ());
 					
 					cell = createCell (current_date.ToString ("dd-MM-yyyy"));
-					table.AddCell (cell, row + 3, col * 2);
+					table.AddCell (cell, row, col * 2);
 					
 					cell = createCell (time.ToString());
-					table.AddCell (cell, row + 3, (col * 2) + 1);
+					table.AddCell (cell, row, (col * 2) + 1);
 				}
-				row += 4;
+				row ++;
 				cell = createCell (" ");
 				cell.Colspan = 6;
-				table.AddCell (cell);
+				table.AddCell (cell, row ++, 0);
 				
 				int seconds_avrg = (minutes_total * 60) / (totaldays+1);
 				TimeSpan total = TimeSpan.FromMinutes (minutes_total);
@@ -176,9 +178,8 @@ namespace Reportero.Reports
 				cell.Colspan = 5;
 				table.AddCell (cell, row, 0);
 				table.AddCell (createCell (TimeSpan.FromSeconds (seconds_avrg).ToString ()), row ++, 5);
-				
-				doc.Add (table);
 			}
+			doc.Add (table);
 			doc.Add (new Paragraph (string.Format ("{0} Vehiculos contabilizados.", vehicles.Count), font_sub2));
 			
 			doc.Close ();
