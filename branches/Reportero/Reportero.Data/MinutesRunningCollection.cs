7ed22f17b7267d844ceaa84d058111415f596829
ricki9@gmail.com <ricki9@gmail.com@6d5072ba-9bd0-11de-9070-0b7171dc4b6e>
2009-11-26 20:45:31 +0000

    Implementing Buffered Report Building
    * Reportero.sln:
    * Reportero.Data/VehicleUser.cs:
    * Reportero.Data/DataCollection.cs:
    * Reportero.Data/SpeedExceedItem.cs:
    * Reportero.Data/ProgressCallback.cs:
    * Reportero.Reports/SpeedReportBar.cs:
    * Reportero.Reports/SpeedListReport.cs:
    * Reportero.Data/MinutesRunningItem.cs:
    * Reportero.Data/Reportero.Data.csproj:
    * Reportero.UI/Widgets/ReportChooser.cs:
    * Reportero.Reports/ActivityReportBar.cs:
    * Reportero.Reports/SpeedGraphicReport.cs:
    * Reportero.Data/SpeedExceedCollection.cs:
    * Reportero.Reports/Reportero.Reports.csproj:
    * Reportero.Data/MinutesRunningCollection.cs:
    * Reportero.Reports/ActivityGraphicReport.cs:

---
+++ branches/Reportero/Reportero.Data/MinutesRunningCollection.cs
@@ -0,0 +1,13 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace Reportero.Data
+{
+	
+	
+	public class MinutesRunningCollection : System.Collections.Generic.List <MinutesRunningItem>
+	{
+		
+	}
+}
