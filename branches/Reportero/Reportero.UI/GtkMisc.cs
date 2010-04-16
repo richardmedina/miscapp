c5f60c419ff25c09a8cc337011a3de98ea463fe3
ricki9@gmail.com <ricki9@gmail.com@6d5072ba-9bd0-11de-9070-0b7171dc4b6e>
2010-04-16 19:40:30 +0000

    * Reportero.userprefs: * Reportero.UI/GtkMisc.cs: * Reportero.UI/Reportero.UI.pidb: * Reportero.Data/Reportero.Data.pidb: * Reportero.Reports/StatisticsReport.cs: * Reportero.UI/Widgets/ReportChooser.cs: * Reportero.Reports/Reportero.Reports.pidb: * Reportero.UI/Widgets/LeadershipMenuPopup.cs:

--- branches/Reportero/Reportero.UI/GtkMisc.cs
+++ branches/Reportero/Reportero.UI/GtkMisc.cs
@@ -15,12 +15,24 @@ namespace Reportero.UI
 			notify.WakeupMain ();
 		}
 		
-		public static string FileSelect (string path, string title, string okbuttonstr)
+		public static string FileSelect (string path, string title, FileChooserAction action)
 		{
 			string filename = string.Empty;
 			
 			Gtk.FileChooserDialog dialog = new Gtk.FileChooserDialog (
-				title, null, FileChooserAction.Open);	
+				title, null, action);
+			
+			dialog.AddButton (Stock.Cancel, ResponseType.Cancel);
+			
+			switch (action) {
+				case FileChooserAction.Open:
+					dialog.AddButton (Stock.Open, ResponseType.Ok);
+				break;
+				
+				case FileChooserAction.Save:				
+					dialog.AddButton (Stock.Save, ResponseType.Ok);
+				break;
+			}
 			
 			if (dialog.Run () == (int) ResponseType.Ok)
 				filename = dialog.Filename;
