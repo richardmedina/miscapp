cb6e33dcb3a1577a11e3030c62f6ccb445e5720a
ricki9 <ricki9@6d5072ba-9bd0-11de-9070-0b7171dc4b6e>
2011-02-17 16:56:17 +0000

    * Registro.cs: * Recomendacion.cs: * PermisoUsuario.cs: * Stprm.DataEx.csproj: * PermisoUsuarioCollection.cs:

---
+++ WebApp/Stprm.DataEx/PermisoUsuarioCollection.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace Stprm.DataEx
+{
+
+
+	public class PermisoUsuarioCollection : List <PermisoUsuario>
+	{
+
+		public PermisoUsuarioCollection ()
+		{
+		}
+	}
+}
