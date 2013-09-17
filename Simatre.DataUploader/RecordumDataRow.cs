ff9f0d73e4cb364b956ff74e9bfafc4685745d63
ricki9@gmail.com <ricki9@gmail.com@6d5072ba-9bd0-11de-9070-0b7171dc4b6e>
2013-09-17 21:29:31 +0000

    Initial check-in of module Simatre.DataUploader

---
+++ Simatre.DataUploader/RecordumDataRow.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Simatre.DataUploader
+{
+	public class RecordumDataRow
+	{
+
+		public string SensorId;
+
+		public DateTime StartDate;
+		public DateTime EndDate;
+
+		public float NO;
+		public float NO2;
+		public float NOX;
+		public float CO;
+		public float O3;
+		public float SO2;
+
+
+		public RecordumDataRow ()
+		{
+		}
+
+		public override string ToString ()
+		{
+			// @RecordumDataRow@aaaa-mm-dd,hh:MM:ss@SensorId@NO@NO2@NOX@CO@O3@SO2
+			return string.Format ("@RecordumDataRow@" 
+			                      + SensorId 
+			                      + "@" + StartDate.ToString (Utils.RecordumDateFormat)
+			                      + "@" + EndDate.ToString (Utils.RecordumDateFormat)
+			                      + "@" + NO.ToString ("0.0000") 
+			                      + "@" + NO2.ToString ("0.0000")
+			                      + "@" + NOX.ToString ("0.0000")
+			                      + "@" + CO.ToString ("0.0000")
+			                      + "@" + O3.ToString ("0.0000")
+			                      + "@" + SO2.ToString ("0.0000")
+			                      );
+		}
+	}
+}
+
---
+++ Simatre.DataUploader/RecordumPollutantType.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Simatre.DataUploader
+{
+	public enum RecordumPollutantType
+	{
+		Unknown,
+		NO,
+		NO2,
+		NOX,
+		CO,
+		O3,
+		SO2
+	} 
+}
+
